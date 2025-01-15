4806a8c0351fe7f922cc832f051c4cd3fddf041c
Adair <adair.fja@gmail.com>
2025-01-14 23:24:24 -0500

    two number

---
+++ two_number.py
@@ -0,0 +1,8 @@
+number1 = (input("write first number "))
+number2 = (input("write second number "))
+if (number1 > number2):
+    print(number1)
+elif (number1 == number2):
+    print("the number are same")
+else:
+    print(number2)
