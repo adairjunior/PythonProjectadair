1191d73c1ff679455fbc1dc0b97197ab8da51200
Adair <adair.fja@gmail.com>
2025-01-15 23:42:19 -0500

    man or woman

---
+++ man_or_woman.py
@@ -0,0 +1,7 @@
+sex = str(input("enter with the sex? M - Man or W - Woman "))
+if (sex == "m" or sex == "M"):
+    print("you are man")
+elif (sex == "W" or sex =="w"):
+    print("you are woman")
+else:
+    print("your sex is unknown")
