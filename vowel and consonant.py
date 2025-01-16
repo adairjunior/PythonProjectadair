defae2791873c627a1afbc019f44728468da5871
Adair <adair.fja@gmail.com>
2025-01-15 23:56:04 -0500

    vowel and consonant

---
+++ vowel and consonant.py
@@ -0,0 +1,5 @@
+vowel = str(input(" enter with 1 lettre "))
+if (vowel == "a" or vowel =="A") or (vowel == "e" or vowel =="E") or(vowel == "i" or vowel =="I") or (vowel == "o" or vowel =="O") or (vowel == "u" or vowel =="U"):
+    print("this lettre is vowel")
+else:
+    print("this is consonant")
