08bd83f3d79d24f2b6d6284d9437c1d216492719
Adair <adair.fja@gmail.com>
2025-01-18 23:15:15 -0500

    big and small number

--- big_small_number.py
+++ big_small_number.py
@@ -4,9 +4,18 @@ number3 = int(input(" enter with the third number "))
 if (number1 > number2) and (number1 > number3) and (number2 > number3):
     print(" number1 is biggest ")
     print(" number3 is smallest ")
-    if ((number2 > number1) and (number2 > number3) and (number3 > number1)):
-        print(" number2 is biggest ")
-        print(" number1 is smallest ")
-        if ((number3 > number1) and (number3 > number2) and (number1 > number2)):
-            print(" number3 is biggest ")
-            print(" number2 is smallest ")
+elif (number1 > number2) and (number1 > number3) and (number3 > number2):
+    print(" number1 is biggest ")
+    print(" number2 is smallest ")
+elif (number2 > number1) and (number2 > number3) and (number3 > number1):
+    print(" number2 is biggest ")
+    print(" number1 is smallest ")
+elif (number2 > number1) and (number2 > number3) and (number1 > number3):
+    print(" number2 is biggest ")
+    print(" number3 is smallest ")
+elif (number3 > number1) and (number3 > number2) and (number1 > number2):
+    print(" number3 is biggest ")
+    print(" number2 is smallest ")
+elif (number3 > number1) and (number3 > number2) and (number2 > number1):
+    print(" number3 is biggest ")
+    print(" number1 is smallest ")
