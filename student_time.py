1b44458325d00eae1a8c4982aed24eff36d0fba4
Adair <adair.fja@gmail.com>
2025-01-20 23:14:50 -0500

    decreasing

---
+++ student_time.py
@@ -0,0 +1,9 @@
+time = str(input(" what is time you study? M-Morning A-Afternoon E-Evening "))
+if (time == "M"):
+    print(" good morning")
+elif (time == "A"):
+    print(" good afternoon")
+elif (time == "E"):
+    print(" good evening")
+else:
+    print(" invalid option")
