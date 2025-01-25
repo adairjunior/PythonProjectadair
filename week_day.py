a8fc730e4c6b1c936c716309f1b4a179e84e15fd
Adair <adair.fja@gmail.com>
2025-01-25 00:17:33 -0500

    week ok

---
+++ week_day.py
@@ -0,0 +1,17 @@
+number = int(input(" enter with the week's day? "))
+if (number == 1):
+    print(" it's sunday")
+elif (number == 2):
+    print(" it's monday")
+elif (number == 3):
+    print(" it's tuesday")
+elif (number == 4):
+    print(" it's wednesday")
+elif (number == 5):
+    print(" it's thursday")
+elif (number == 6):
+    print(" it's friday")
+elif (number == 7):
+    print(" it's saturday")
+else:
+    print(" don't have this day")
