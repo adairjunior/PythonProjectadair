781a544c51c73c0fb932cdf9e0fd2231c15d8a93
Adair <adair.fja@gmail.com>
2025-01-28 00:18:24 -0500

    triangle ok

--- triangle.py
+++ triangle.py
@@ -3,3 +3,11 @@ side2 = int(input(" what's with the second side's triangle? "))
 side3 = int(input(" what's with the third side's triangle? "))
 if (side1 + side2) > side3 or (side1 + side3) > side2 or (side2 + side3) > side1:
     print(" i's triangle")
+    if (side1 == side2) and (side1 == side3):
+        print(" it's triangle's equileteral ")
+    elif ((side1 == side2) and (side1 != side3)) or ((side1 == side3) and (side1 != side2)) or ((side2 == side3) and (side2 != side1)):
+        print(" it's isosceles")
+    elif (side1 != side2) and (side1 != side3) and (side2 != side3):
+        print(" it's triangle's scalene")
+    else:
+        print(" it isn't triangle ")
