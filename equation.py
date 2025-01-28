48ff10142fa1f3783c1e6f382a4a6590d4c38dc0
Adair <adair.fja@gmail.com>
2025-01-28 00:29:17 -0500

    the number is ok

---
+++ equation.py
@@ -0,0 +1,5 @@
+a = int(input(" what's the number 'a' in the equation? "))
+b = int(input(" what's the number 'b' in the equation? "))
+c = int(input(" what's the number 'c' in the equation? "))
+if (a == 0):
+    print(" don't second degree equation ")
