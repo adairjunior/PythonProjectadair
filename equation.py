53473c8fc2a9635c8a93f007904881e579d3210e
Adair <adair.fja@gmail.com>
2025-01-29 00:24:29 -0500

    don't good

--- equation.py
+++ equation.py
@@ -1,5 +1,6 @@
 a = int(input(" what's the number 'a' in the equation? "))
 b = int(input(" what's the number 'b' in the equation? "))
 c = int(input(" what's the number 'c' in the equation? "))
-if (a == 0):
+x=0
+if (a * (x**2) + (b * x) + c) == 0:
     print(" don't second degree equation ")
