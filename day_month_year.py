7867a5dbbaa376925a21d170a492b980aa7084ee
Adair <adair.fja@gmail.com>
2025-02-02 23:25:21 -0500

    valid date ok

--- day_month_year.py
+++ day_month_year.py
@@ -1,3 +1,11 @@
 day = int(input(" type the day - dd? "))
 month = int(input(" type the month - mm? "))
-year = int(input(" type the year - yy? "))
+year = int(input(" type the year - yyyy? "))
+if (day >= 1) and (day <= 31):
+    if (month >= 1) and (month <= 12):
+        print(" valid date")
+        print(" The day is",day,". The month is", month,". The year is", year,". ")
+    else:
+        print(" invalid month")
+else:
+    print(" invalid date")
